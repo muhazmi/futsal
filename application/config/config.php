0342b974d2d805d3634c90df9f15bf5b8c21de96
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2024-10-11 09:09:20 +0700

    feat: prepare for using php web server

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/futsal';
+$config['base_url'] = 'http://localhost:8080';
 
 /*
 |--------------------------------------------------------------------------
